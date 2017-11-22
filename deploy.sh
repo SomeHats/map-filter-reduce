dd4176a3e144de7130596113d34a4a0af832e8ad
Alex Dytrych <alex@dytry.ch>
2017-11-22 12:47:43 +0000

    remove extra stuff from deployed folder

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ cp -r out/ $work_dir
 rm -rf out/
 git checkout gh-pages
 git merge master
-rm -rf pages static scripts LICENSE next.config.js package-lock.json package.json README.md
+rm -rf pages static scripts LICENSE next.config.js package-lock.json package.json README.md deploy.sh .babelrc
 cp -r $work_dir/* .
 touch .nojekyll
 git add -A
