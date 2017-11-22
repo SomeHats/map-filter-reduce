871e2177441070e3804269b2ec511297ea1b1f90
Alex Dytrych <alex@dytry.ch>
2017-11-22 12:38:29 +0000

    add deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ux
+
+current_branch=`git symbolic-ref HEAD`
+current_commit=`git log -1 --oneline`
+
+if [ $current_branch != "refs/heads/master" ]; then
+  echo "Not on branch 'master', cannot deploy"
+  exit
+fi
+
+if [[ `git status --porcelain` ]]; then
+  echo "Uncommitted changes, cannot deploy"
+  exit
+fi
+
+work_dir=`mktemp -d`
+
+npm run build
+cp -r out/ $work_dir
+rm -rf out/
+git checkout gh-pages
+git merge src
+rm -rf pages static scripts LICENSE next.config.js package-lock.json package.json README.md
+cp -r $work_dir/* .
+touch .nojekyll
+git add -A
+git commit -m "Build from $current_commit"
+git push origin gh-pages --force
+git checkout master
