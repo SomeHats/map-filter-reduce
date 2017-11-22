8fe66c57c37b9f63a2d80341f24b91269785d7e8
Alex Dytrych <alex@dytry.ch>
2017-11-22 12:39:31 +0000

    Build from c96c87c add deploy scripts

--- README.md
+++
@@ -1,2 +0,0 @@
-# map-filter-reduce
-Interactive explanation of map, filter, and reduce in JS
--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  exportPathMap: () => ({
-    '/': { page: '/' },
-  }),
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/out' : '',
-};
